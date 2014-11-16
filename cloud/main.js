4d88adac83e8522a9a804ee83bcd923f66f552b0
Carlos Renoj <renojpa@gmail.com>
2014-11-15 20:59:01 -0500

    initial commit

---
+++ cloud/main.js
@@ -0,0 +1,17 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+
+Parse.Cloud.beforeSave("TestObject", function(request, response) {
+  if (request.object.get("foo") < 1) {
+    response.error("you cannot give less than one star");
+  } else if (request.object.get("foo") > 5) {
+    response.error("you cannot give more than five stars");
+  } else {
+   response.error("unknow property....!");
+  }
+});
