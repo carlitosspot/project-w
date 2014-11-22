7b2ca1254d9824825dc970281f21d5301ee71558
Carlos Renoj <renojpa@gmail.com>
2014-11-22 02:02:33 -0500

    updates main

--- cloud/main.js
+++ cloud/main.js
@@ -2,22 +2,46 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
 });
 
 
-Parse.Cloud.beforeSave("TestObject", function(request, response) {
-  if (request.object.get("foo") < 1) {
-    response.error("you cannot give less than one star");
-  } else if (request.object.get("foo") > 5) {
-    response.error("you cannot give more than five stars");
-  } else {
-   response.error("unknow property....!!");
-  }
+Parse.Cloud.beforeSave("Event", function(request, response) {
+	if (request.object.get("foo") < 1) {
+	response.error("you cannot give less than one star");
+	} else if (request.object.get("foo") > 5) {
+	response.error("you cannot give more than five stars");
+	} else {
+	response.error("unknow property....!!");
+	}
 });
 
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
-   var name = request.object.get("username");
-		response.error("cannot save user: "+name);
-  
+
+	var require = function(name, request){
+					if( !request.object.get(name)  && request.object.get(name).length <= 0 )
+						return 'Missing value for '+ name;
+					return '';
+				};
+
+	var email = request.object.get("email");
+		request.object.set("email", email.toLowerCase());
+
+	var query = new Parse.Query(Parse.User);
+	    query.equalTo('username', request.object.get("username"));
+	    query.equalTo('email', request.object.get("email"));
+		query.find({
+		  success: function(results) {
+		    response.success();
+		  },
+
+		  error: function(error) {
+		    //response.error('Username: '+request.object.get('username')+' is already being used');
+		    response.error(error);
+		  }
+		});
+
+	// var name = request.object.get("username");
+	// response.error("cannot save user: "+name);
+
 });
